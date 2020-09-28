6cbb8fef29d8d4832783145b8e76e4d9f8e23923
lukecheetham <71508800+lukecheetham@users.noreply.github.com>
2020-09-28 16:57:31 +0100

    Add files via upload

---
+++ euler V2/CalculateSumOfMultiples.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace euler
+{
+    public class CalculateSumOfMultiples
+    {
+        public int Calculate(IEnumerable<int> range)
+        {
+            int sum = 0;
+
+            foreach (int i in range)
+            {
+                if ((i % 3) == 0 || (i % 5) == 0)
+                {
+                    Console.WriteLine($"{i} is a multiple");
+
+                    sum += i;
+
+                }
+
+            }
+            return sum;
+        }
+    }
+}
---
+++ euler V2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Schema;
+
+namespace euler
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> range = Enumerable.Range(1, 999);
+            var multiples = new CalculateSumOfMultiples();
+            var sumOfMultiples = multiples.Calculate(range);
+            Console.WriteLine($"The total sum of the multiples is: {sumOfMultiples}");
+        }
+    }
+    
+}
